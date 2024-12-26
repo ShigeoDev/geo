41d0ecc51906fdf928c4dd8232ef6b94d024c318
ShigeoDev <84653162+ShigeoDev@users.noreply.github.com>
2024-12-26 00:26:18 -0800

    use const not var

--- app/components/ParentComponent.tsx
+++ app/components/ParentComponent.tsx
@@ -5,16 +5,16 @@ import SubmitButtion from './SubmitButton';
 import Scoreboard from './Scoreboard';
 
 export function getDistanceFromLatLonInKm(lat1: number, lon1: number, lat2: number, lon2: number) {
-  let R = 6371; // Radius of the earth in km
-  let dLat = deg2rad(lat2 - lat1);  // deg2rad below
-  let dLon = deg2rad(lon2 - lon1);
-  let a =
+  const R = 6371; // Radius of the earth in km
+  const dLat = deg2rad(lat2 - lat1);  // deg2rad below
+  const dLon = deg2rad(lon2 - lon1);
+  const a =
     Math.sin(dLat / 2) * Math.sin(dLat / 2) +
     Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
     Math.sin(dLon / 2) * Math.sin(dLon / 2)
     ;
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  var d = R * c; // Distance in km
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const d = R * c; // Distance in km
   return d;
 }
 
